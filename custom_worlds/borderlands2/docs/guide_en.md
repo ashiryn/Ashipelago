0cc4ccb0770389a6e48530614f8d610e5b9d6668
Joe Thompson <azyle0823@gmail.com>
2026-01-10 20:04:35 -0500

    Reverting worlds back to the worlds folder, too much of a headache

--- custom_worlds/borderlands2/docs/guide_en.md
+++
@@ -1,7 +0,0 @@
-# Borderlands 2 Setup Guide
-TODO: content here
-## Required Software
-
-## Installation Procedures (Windows)
-
-## Joining a MultiWorld Game
--- custom_worlds/rac3/test/__init__.py
+++
@@ -1,6 +0,0 @@
-from test.bases import WorldTestBase
-from worlds.rac3 import GAME_TITLE_FULL
-
-
-class RAC3TestBase(WorldTestBase):
-    game = GAME_TITLE_FULL
