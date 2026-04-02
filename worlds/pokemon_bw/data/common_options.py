f12a7950b39c54089a5bb2362a9e79c8cb191f96
ashiryn <azyle0823@gmail.com>
2026-04-02 12:03:29 -0400

    Extracted custom controls out of options file to hopefully fix unit test

---
+++ worlds/pokemon_bw/data/common_options.py
@@ -0,0 +1,24 @@
+import typing
+
+from Options import OptionSet, OptionError
+
+
+class CasefoldOptionSet(OptionSet):
+    valid_keys_casefold = True
+
+    def __init__(self, value: typing.Iterable[str]):
+        self.value = set(val.casefold() for val in value)
+        super(OptionSet, self).__init__()
+
+    def __contains__(self, item: str):
+        return item.casefold() in self.value
+
+    def verify_keys(self) -> None:
+        if self.valid_keys:
+            dataset = set(word.casefold() for word in self.value)
+            extra = dataset - set(key.casefold() for key in self._valid_keys)
+            if extra:
+                raise OptionError(
+                    f"Found unexpected key {', '.join(extra)} in {getattr(self, 'display_name', self)}. "
+                    f"Allowed keys: {self._valid_keys}."
+                )
--- worlds/pokemon_bw/options.py
+++ worlds/pokemon_bw/options.py
@@ -8,30 +8,10 @@ import settings
 from BaseClasses import PlandoOptions
 from Options import (Choice, PerGameCommonOptions, OptionSet, Range, Toggle,
                      PlandoTexts, OptionError, Option, OptionCounter, StartInventoryPool)
+from worlds.pokemon_bw.data.common_options import CasefoldOptionSet
 
 if typing.TYPE_CHECKING:
     from worlds.AutoWorld import World
-
-
-class CasefoldOptionSet(OptionSet):
-    valid_keys_casefold = True
-
-    def __init__(self, value: typing.Iterable[str]):
-        self.value = set(val.casefold() for val in value)
-        super(OptionSet, self).__init__()
-
-    def __contains__(self, item: str):
-        return item.casefold() in self.value
-
-    def verify_keys(self) -> None:
-        if self.valid_keys:
-            dataset = set(word.casefold() for word in self.value)
-            extra = dataset - set(key.casefold() for key in self._valid_keys)
-            if extra:
-                raise OptionError(
-                    f"Found unexpected key {', '.join(extra)} in {getattr(self, 'display_name', self)}. "
-                    f"Allowed keys: {self._valid_keys}."
-                )
 
 
 class GameVersion(Choice):
